edeb0ecc9d9a392a372dd81c1f4a1a592ae6e697
Omm Sahoo <osahoo9178@gmail.com>
2025-11-10 18:49:02 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,10 @@
 
 // This file is intentionally left blank. It will be populated with your Firebase project's configuration object by the Firebase CLI.
-export const firebaseConfig = {};
-
-    
+export const firebaseConfig = {
+  "projectId": "studio-7916238413-b46c3",
+  "appId": "1:146675080337:web:4bd1940751a5fbfad0f330",
+  "apiKey": "AIzaSyDcx8_c8qN3L3n6_HBRZl2p3UiHcNFDGs0",
+  "authDomain": "studio-7916238413-b46c3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "146675080337"
+};
